73eef1736588333bcebaf241dfc688653c718d01
NickKline9931 <nickkline9931@gmail.com>
2023-02-20 09:53:28 -0500

    Style posts on index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
-  resources :posts
+  resources :posts, except: [:show]
 
   root "posts#index"
 
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
     get 'sign_in', to: 'users/sessions#new'
     get 'sign_out', to: 'users/sessions#destroy'
   end
+
+  delete 'posts/:id', to: 'posts#destroy'
   
 end
